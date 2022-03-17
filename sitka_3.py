1a92c74df9377ba923c04772b35d3f82a72a1c25
ryanleckich <leckich9@gmail.com>
2022-03-16 23:54:41 -0500

    done

--- sitka_3.py
+++ sitka_3.py
@@ -1,15 +1,19 @@
 import csv
 from datetime import datetime
 
-weather = open("death_valley_2018_simple.csv", "r")
 
+weather = open("sitka_weather_2018_simple.csv", "r")
 csv_file = csv.reader(weather, delimiter=",")
+
 
 header_row = next(csv_file)
 
+
+print(type(header_row))
+
+
 for index, column_header in enumerate(header_row):
     print(index, column_header)
-
 
 highs = []
 dates = []
@@ -17,19 +21,11 @@ lows = []
 
 
 for row in csv_file:
+    highs.append(int(row[5]))
 
-    try:
-        current_date = datetime.strptime(row[2], "%Y-%m-%d")
-        high = int(row[4])
-        low = int(row[5])
-
-    except ValueError:
-
-        print(f"Missing data for {current_date}")
-    else:
-        highs.append(high)
-        lows.append(low)
-        dates.append(current_date)
+    current_date = datetime.strptime(row[2], "%Y-%m-%d")
+    dates.append(current_date)
+    lows.append(int(row[6]))
 
 
 print(highs)
@@ -38,16 +34,33 @@ print(dates)
 
 import matplotlib.pyplot as plt
 
+
 fig = plt.figure()
 plt.plot(dates, highs, c="red")
 
 plt.plot(dates, lows, c="blue")
+
 
 plt.fill_between(dates, highs, lows, facecolor="blue", alpha=0.3)
 plt.title("Daily low and high temperatures - 2018", fontsize=16)
 plt.xlabel("Month of July 2018")
 plt.ylabel("Temperatures (F)", fontsize=16)
 plt.tick_params(axis="both", which="major", labelsize=16)
-# shows graph
+
+
 fig.autofmt_xdate()
+plt.show()
+
+
+plt.subplot(2, 1, 1)
+plt.plot(dates, highs, c="red")
+plt.title("Highs")
+
+plt.subplot(2, 1, 2)
+plt.plot(dates, lows, c="blue")
+plt.title("Lows")
+
+plt.suptitle("Highs and Lows of Sitka, Alaska 2018")
+
+
 plt.show()
